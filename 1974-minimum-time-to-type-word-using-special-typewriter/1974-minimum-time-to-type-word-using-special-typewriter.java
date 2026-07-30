56951f28940abd7f58019e66c5aa45d944512a68
Vivek Pawar <160049777+vivekpawar3010@users.noreply.github.com>
2026-07-30 13:01:42 +0530

    Time: 1 ms (61.7%), Space: 42.4 MB (97.08%) - LeetHub

--- 1974-minimum-time-to-type-word-using-special-typewriter/1974-minimum-time-to-type-word-using-special-typewriter.java
+++ 1974-minimum-time-to-type-word-using-special-typewriter/1974-minimum-time-to-type-word-using-special-typewriter.java
@@ -4,10 +4,17 @@ class Solution {
         char prev = 'a';
         for (int i = 0; i < word.length(); ++i) {
             char cur = word.charAt(i);
-            int diff = Math.abs(cur - prev);
-            cnt += Math.min(diff, 26 - diff);
+            int diff = abs(cur - prev);
+            cnt += min(diff, 26 - diff);
             prev = cur;
         }
         return cnt;
+    }
+
+    private int abs(int n){
+        return n > 0? n:-n;
+    }
+    private int min(int a, int b){
+        return a <b? a:b;
     }
 }
