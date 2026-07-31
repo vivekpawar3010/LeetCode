0c768876ab13450ea5a69d1910797d7bc919d55e
Vivek Pawar <160049777+vivekpawar3010@users.noreply.github.com>
2026-07-31 10:48:44 +0530

    Time: 1 ms (100%), Space: 46.3 MB (69.03%) - LeetHub

---
+++ 2710-remove-trailing-zeros-from-a-string/2710-remove-trailing-zeros-from-a-string.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String removeTrailingZeros(String num) {
+        int n = num.length() - 1;
+
+        while(n >= 0 && num.charAt(n) == '0'){
+            n--;
+        }
+
+        return num.substring(0, n + 1);
+    }
+}
