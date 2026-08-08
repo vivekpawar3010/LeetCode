d889ccc2da2f4debef147adbe2f146e4d68d84a5
Vivek Pawar <160049777+vivekpawar3010@users.noreply.github.com>
2026-08-08 09:18:54 +0530

    Time: 1 ms (99.86%), Space: 43.2 MB (36.82%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -1,18 +1,30 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-        Map<Character, Integer> freq = new HashMap<>();
+        // Map<Character, Integer> freq = new HashMap<>();
 
-        for(char ch : t.toCharArray()){
-            freq.put(ch, freq.getOrDefault(ch, 0) + 1);
+        // for(char ch : t.toCharArray()){
+        //     freq.put(ch, freq.getOrDefault(ch, 0) + 1);
+        // }
+
+        // for(char ch : s.toCharArray()){
+        //     freq.put(ch, freq.get(ch) - 1);
+        //     if(freq.get(ch) == 0) {
+        //         freq.remove(ch);
+        //     }
+        // }        
+
+        // return (char) (freq.keySet().toArray()[0]);
+
+        // ----------- BEST WAY IS USE XOR OPRATION -----------//
+
+        char ans = 0;
+        for(char ch: s.toCharArray()){
+            ans ^= ch;
+        }
+        for(char ch: t.toCharArray()){
+            ans ^= ch;
         }
 
-        for(char ch : s.toCharArray()){
-            freq.put(ch, freq.get(ch) - 1);
-            if(freq.get(ch) == 0) {
-                freq.remove(ch);
-            }
-        }        
-
-        return (char) (freq.keySet().toArray()[0]);
+        return ans;
     }
 }
