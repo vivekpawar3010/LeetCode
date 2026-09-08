9a4e02ef8e0a1124241aea5ea995f770e3d5e61b
Vivek Pawar <160049777+vivekpawar3010@users.noreply.github.com>
2026-09-08 11:21:00 +0530

    Time: 0 ms (100%), Space: 42.5 MB (57.08%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -1,5 +1,8 @@
 class Solution {
     public int countCommas(int n) {
-        return (n < 999)? 0:(n - 999);
+        if(n<1000){
+            return 0;
+        }
+        return n - 999;
     }
 }
